06b07b56248a2f1e58ca1887b6a1436f461cee77
Alt Ais <suiutbkva@gmail.com>
2020-02-12 17:47:52 -0600

    changes key

--- key_pair.tf
+++ key_pair.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "terraform" {
   key_name   = "terrafrom"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDCfc4E9UvQak3lvtPxFrBFbBezdJSvyeRfBa0q5y+ntXTuTFyblNezKz0MWI+z+Qy7oIbTu8QgldCc/8Py54z4TB8/Dnz+pI1HhMNnYzA1Ti5OvSeKTHxOmGc4AA/pCKRTDl+zPhPVnUl9fcsWiIIGXoIVgiQ0Uc4u4QlgYUQPp0s3frlbPtRO9v6dDvkTKGLtnukwP0GpIA1eWuVXu/2jbf4huiH3TcDymEL514CusXhKDggE6LxtNH3usMUSQnn6k729wzbN7QuFU4wYe9kzIvFyTU85sQiWeV/Shp73QicZD8hx+0CfKFHsVJQQ8dFqijd/QGyXIF3kyt/Oln6V root@ip-172-31-47-12.eu-west-1.compute.internal"
+  public_key = "${file("~/ssh/id_rsa.pub")}"
 }
