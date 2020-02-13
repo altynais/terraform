fd1f3f9266052d9ff126a6053ed732425e616552
Alt Ais <suiutbkva@gmail.com>
2020-02-12 18:05:13 -0600

    security group added

---
+++ sec_group.tf
@@ -0,0 +1,13 @@
+resource "aws_security_group" "sec1" {
+  name        = "sec1"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-311f294b"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = "0.0.0.0/0"
+  }
+
+}
