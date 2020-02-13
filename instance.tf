d4bce6e2e60a5652b75d16de6fcfb1251c14d6e0
Alt Ais <suiutbkva@gmail.com>
2020-02-12 18:34:51 -0600

    tags added

--- instance.tf
+++ instance.tf
@@ -2,6 +2,11 @@ resource "aws_instance" "web1" {
     ami="ami-0b898040803850657"
     instance_type="t2.micro" 
     key_name = "terraform" 
+
+    tags = {
+        Name = "Wed Server"
+        Environment = "Prod"
+  }
 }
 
 
