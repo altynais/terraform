bf49de799b41f47a3b17b8508fdd4810750f52c4
Alt Ais <suiutbkva@gmail.com>
2020-02-14 21:26:54 -0600

    fix

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        dynamodb_table = "dev_state_lock"
+        # dynamodb_table = "dev_state_lock"
         bucket = "terraform-altynais"
         region = "us-east-1"
         key = "dev.tfstate"
